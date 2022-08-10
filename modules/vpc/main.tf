ca00b94c78cb9bff31931e184ee75bf170c90a9c
Esteban Benavides <78662995+tban88@users.noreply.github.com>
2022-08-10 14:16:20 -0600

    fix security ingress and outbound rules

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -110,17 +110,19 @@ resource "aws_security_group" "default" {
   ]
 
     ingress {
-    from_port = "0"
-    to_port   = "0"
-    protocol  = "-1"
-    self      = true
+    description = "SSH access"
+    from_port = "22"
+    to_port   = "22"
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = "0"
-    to_port   = "0"
-    protocol  = "-1"
-    self      = "true"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
