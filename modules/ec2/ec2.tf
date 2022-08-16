0cd01a90e87cd9ee2d4bd7520568c0e7f0648aa1
Esteban Benavides <78662995+tban88@users.noreply.github.com>
2022-08-16 17:36:44 -0600

    add tags

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -13,18 +13,24 @@ provider "aws" {
 
 ######################## RESOURCES: PROD ########################
 
+locals {
+  ec2_instance = aws_instance.prod_ec2_jenkins.id
+}
 resource "aws_instance" "prod_ec2_jenkins" {
   ami = "ami-052efd3df9dad4825"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   associate_public_ip_address = true
   subnet_id = var.subnet_id
   key_name = "test-devops"
   vpc_security_group_ids = ["${var.security_group_id}"]
   user_data = file("./modules/ec2/user_data/jenkins.sh")
+  tags = {
+    "Name" = var.ec2_name
+  }
 }
 
 resource "aws_lb_target_group_attachment" "tg_ec2_jenkins_asoc" {
   target_group_arn = var.tg_arn
-  target_id        = aws_instance.prod_ec2_jenkins.id
-  port             = 8080
+  target_id        = local.ec2_instance
+  port             = var.port
 }
