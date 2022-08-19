b8e7c20d0e5477ea6ca5993f3edf61dddebb91f9
Esteban Benavides <78662995+tban88@users.noreply.github.com>
2022-08-19 17:45:05 -0600

    removed locals and replaced variables

--- modules/alb/nonprod/alb.tf
+++ modules/alb/nonprod/alb.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_lb" "nonprod_lb" {
   name = var.lb_name
   subnets = var.internal == false ? var.nonprod_pub_subnets : var.nonprod_prv_subnets
-  security_groups = local.nonprod_default_sg
+  security_groups = var.nonprod_default_sg
   internal = var.internal
   load_balancer_type = var.lb_type
 }
--- modules/alb/prod/alb.tf
+++ modules/alb/prod/alb.tf
@@ -11,21 +11,12 @@ provider "aws" {
   region = var.region
 }
 
-######################## LOCALS: PROD ########################
-
-locals {
-  prod_prv_subnets = var.prod_prv_subnets
-  prod_pub_subnets = var.prod_pub_subnets
-  prod_default_sg = var.prod_default_sg
-  prod_vpc = var.prod_vpc
-}
-
 ######################## RESOURCES: PROD ########################
 
 resource "aws_lb" "prod_lb" {
   name = var.lb_name
   subnets = var.internal == false ? var.prod_pub_subnets : var.prod_prv_subnets
-  security_groups = local.prod_default_sg
+  security_groups = var.prod_default_sg
   internal = var.internal
   load_balancer_type = var.lb_type
 }
