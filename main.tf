3a91dfa3d866c7e933e2d3014b0c0f4f1566e8c8
Esteban Benavides <78662995+tban88@users.noreply.github.com>
2022-08-30 12:22:31 -0600

    akpi baseline

--- main.tf
+++ main.tf
@@ -4,22 +4,9 @@ terraform {
 
 provider "aws" {
   region = var.main_region
-}
-/*
-data "aws_security_group" "prod_default_sg" {
-  filter {
-    name = "tag:Name"
-    values = ["PROD-DEFAULT-SG"]
-  }
+  profile = terraform
 }
 
-data "aws_subnet" "data_prod_pub_subnet_A_id" {
-  filter {
-    name = "tag:Name"
-    values = ["PROD-PUB-SN-A"]
-  }
-}
-*/
 locals {
 
   prod_prv_subnets = ["${module.vpc.prod_prv_subnet_A_data.id}", "${module.vpc.prod_prv_subnet_B_data.id}"]
@@ -34,26 +21,6 @@ locals {
   prod_alb_public_arn = module.prod_pub_lb.lb_arn
   pub_lb_dns = module.prod_pub_lb.dns_url
 
-  #To use in SG ingress/egress rules (Dynamic Blocks)
-  sg_ingress = [{
-    port = 80
-    description = "SG Description - test"
-    protocol = "tcp"
-    cidr_blocks = "0.0.0.0/0"
-  },
-  {
-    port = 443
-    description = "SG Description - test"
-    protocol = "tcp"
-    cidr_blocks = "0.0.0.0/0"
-  }]
-
-  sg_egress = [{
-    port = 0
-    protocol = "-1"
-    cidr_blocks = "0.0.0.0/0"
-    ipv6_cidr_blocks = "::/0"
-  }]
 }
 
 #Creates baseline networking resources, for new ones, refer to vpc module 
@@ -72,98 +39,19 @@ module "prod_pub_lb" {
   internal = false
 }
 
-module "prod_tg_jenkins" {
+module "prod_tg_devops" {
   source = "./modules/target_group"
-  tg_name = "JENKINS-TG"
-  port = 8080
+  tg_name = "DEVOPS-TG"
+  port = 80
   vpc_id = module.vpc.prod_vpc_data.id
-  health_path = "/login"
-  health_port = 8080
+  health_path = "/"
+  health_port = 80
   health_protocol = "HTTP"
 }
 
 module "pub_lb_listener" {
   source = "./modules/alb/listener"
   lb_arn = module.prod_pub_lb.lb_arn
-  target_arn = module.prod_tg_jenkins.tg_arn
+  target_arn = module.prod_tg_devops.tg_arn
 }
 
-module "ec2-jenkins" {
-  source = "./modules/ec2"
-  vpc_id = module.vpc.prod_vpc_data.id
-  subnet_id = module.vpc.prod_pub_subnet_A_data.id
-  security_group_id = module.vpc.prod_df_sg_data.id
-  ami_id = "ami-052efd3df9dad4825" #ubuntu 22.04
-  user_data = "./modules/ec2/user_data/jenkins.sh"
-  ec2_name = "JENKINS"
-  public_ip = true
-}
-
-module "ec2-jenkins-prv" {
-  source = "./modules/ec2"
-  vpc_id = module.vpc.prod_vpc_data.id
-  subnet_id = module.vpc.prod_prv_subnet_B_data.id
-  security_group_id = module.vpc.prod_df_sg_data.id
-  ami_id = "ami-052efd3df9dad4825" #ubuntu 22.04
-  #user_data = "./modules/ec2/user_data/jenkins.sh"
-  ec2_name = "JENKINS-PRV"
-  public_ip = false
-}
-
-module "jenkins_tg_asoc" {
-  source = "./modules/target_asoc"
-  tg_arn = module.prod_tg_jenkins.tg_arn
-  ec2_id = module.ec2-jenkins.ec2_id
-  port = 8080
-}
-
-module "ec2-openvpn" {
-  source = "./modules/ec2"
-  vpc_id = module.vpc.prod_vpc_data.id
-  subnet_id = module.vpc.prod_pub_subnet_A_data.id
-  security_group_id = module.vpc.prod_df_sg_data.id
-  ami_id = "ami-08d4ac5b634553e16" #ubuntu 20.04
-  user_data = "./modules/ec2/user_data/openvpn.sh"
-  ec2_name = "OPENVPN"
-  public_ip = true
-}
-
-## TEST BLOCKS
-
-module "new_vpc" {
-  source = "./modules/vpc"
-  cidr_block = "192.168.0.0/16"
-  vpc_name = "devops"
-}
-
-module "test-sg" {
-  source = "./modules/security_group"
-  sg_name = "test-DEVOPS"
-  sg_description = "Devops test security group"
-  vpc_id = module.new_vpc.vpc_id
-  ingress_object_rules = local.sg_ingress
-  egress_object_rules = local.sg_egress
-}
-module "test-subnet-pub" {
-  source = "./modules/subnet/public"
-  sn_name = "test-public"
-  vpc_id = module.new_vpc.vpc_id
-  cidr_block = "192.168.100.0/24"
-  az_name = "us-east-1c"
-  igw_id = module.new_vpc.igw_id
-}
-
-module "new_nat" {
-  source = "./modules/nat"
-  nat_name = "test"
-  subnet_id = module.test-subnet-pub.subnet_id
-}
-
-module "test-subnet-prv" {
-  source = "./modules/subnet/private"
-  sn_name = "test-private"
-  vpc_id = module.new_vpc.vpc_id
-  cidr_block = "192.168.200.0/24"
-  az_name = "us-east-1c"
-  nat_id = module.new_nat.nat_id
-}
